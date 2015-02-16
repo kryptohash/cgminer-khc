3edbbffed6df37230ee5e831410f178215eb0e36
kryptohash <kryptohash@gmail.com>
2015-02-16 09:16:47 -0600

    Added new version2 algo
    KSHAKE320v2

--- kryptohash.h
+++ kryptohash.h
@@ -3,9 +3,10 @@
 
 #include "sha3/sha3.h"
 
-#define KSHAKE320_L             (320)  // Length in bits
-#define KRATE                   (960)  // Keccak rate in bits
-#define KPROOF_OF_WORK_SZ (546*KRATE)  // KryptoHash Proof of Work Size in bits
+#define KSHAKE320_L       (320)  // Length in bits
+#define KPOW_MUL          (546)  // How many Keccak blocks the PoW contains
+#define KRATE             (KSHAKE320_R / 8)  // Keccak rate in bytes
+#define KPROOF_OF_WORK_SZ (KPOW_MUL*KRATE)  // KryptoHash Proof of Work Block Size in bytes
 
 struct uint320
 {
